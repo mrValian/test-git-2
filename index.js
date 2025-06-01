783914e0609ad22d5a633c1084437ba34e56138d
Aleksandr Fartushniy <valian55029@gmail.com>
2025-06-01 12:20:55 +0300

    edit main page

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ console.log("test");
 console.log("test");
 console.log("test");
 console.log("test");
+
+let age = 20;
+
+if (age >= 18) {
+    console.log("ok");
+} else {
+    console.log("not ok");
+}
