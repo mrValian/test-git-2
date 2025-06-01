3374c3e7b8134924932138c8fd3be0eb807ada91
Aleksandr Fartushniy <valian55029@gmail.com>
2025-06-01 12:24:56 +0300

    change some info

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ console.log("test");
 console.log("test");
 console.log("test");
 console.log("test");
+
+let age = 6;
+if (age > 7) {
+    console.log("brrr");
+} else {
+    console.log("vrrr");
+}
